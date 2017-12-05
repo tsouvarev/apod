791aa860df318574de886d1f8f78da3e859cc3f6
Ivan Tsouvarev <tsouvarev@gmail.com>
2017-12-05 10:37:39 +0300

    Handle Vimeo videos in astronet parser

--- astronet.py
+++ astronet.py
@@ -11,6 +11,7 @@ from utils import send_to_slack
 ASTRONET_PREVIEW_URL = 'http://www.astronet.ru/db/apod.html'
 ASTRONET_PRINT_URL = 'http://www.astronet.ru/db/print/msg/%s/'
 YOUTUBE_VIDEO_URL = 'https://youtube.com/watch?v=%s'
+VIMEO_VIDEO_URL = 'https://vimeo.com/%s/'
 
 
 def get_preview_for_date(dt):
@@ -31,21 +32,32 @@ def get_details_from_preview(preview):
     return requests.get(details_url).content.decode('cp1251')
 
 
+def get_image_from_details(details):
+    images = details.select('div[align="center"] img')
+    if images:
+        return images[0].attrs['src']
+
+    videos = details.select('div[align="center"] > center > iframe')
+    if videos:
+        video_url = videos[0].attrs['src']
+        parts = urlparse(video_url).path.split('/')
+
+        if 'vimeo' in video_url:
+            vimeo_video_id = parts[-1]
+            return VIMEO_VIDEO_URL % vimeo_video_id
+
+        if 'embed' in video_url:
+            youtube_video_id = parts[2]
+            return YOUTUBE_VIDEO_URL % youtube_video_id
+
+    raise NotImplementedError("Can't parse image")
+
+
 def parse_details(details):
     bs = BeautifulSoup(details, "html.parser")
 
     title = bs.select('div font b')[0].get_text()
-    images = bs.select('div[align="center"] img')
-    if images:
-        image = images[0].attrs['src']
-    else:
-        videos = bs.select('div[align="center"] > center > iframe')
-        video_url = videos[0].attrs['src']
-        parts = urlparse(video_url).path.split('/')
-        if 'embed' in parts:
-            video_id = parts[2]
-            image = YOUTUBE_VIDEO_URL % video_id
-
+    image = get_image_from_details(bs)
     content = bs.select('#content')[0]
 
     for tag in ['table', 'div', 'b', 'p']:
